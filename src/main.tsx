9a497b48e36ef454b11a1c2a82274425a3843c3d
Star Lederer <germans.lederers@gmail.com>
2023-05-02 01:16:10 +0200

    feat: implement MUI components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
+import CssBaseline from '@mui/material/CssBaseline';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,12 +1,24 @@
 import { Link, Outlet } from "react-router-dom"
+import { Button, AppBar, Box, Container } from "@mui/material"
+import { FC } from "react"
+
+const Lnk: FC<{
+  to: string,
+  children: string
+}> = (props) => <Button variant="outlined" sx={{ color: "Background" }} component={Link} {...props}>{props.children}</Button>
 
 const Main = () => {
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Box display="flex" gap={1} py={2} >
+            <Lnk to="/">Home</Lnk>
+            <Lnk to="/about">About</Lnk>
+          </Box>
+        </Container>
+      </AppBar>
+
       <Outlet />
     </>
   )
